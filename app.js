48a6a04717f489d89d723c320c2cf658f25f7f45
Radek885 <107857808+Radek885@users.noreply.github.com>
2025-06-08 20:30:06 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const cors = require("cors");
+const allowedOrigins = ["https://paas-blog-frontend.onrender.com"];
+
+const { Pool } = require("pg");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST"],
+}));
+
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+(async () => {
+  try {
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS posts (
+      id SERIAL PRIMARY KEY,
+      title TEXT NOT NULL,
+      content TEXT NOT NULL,
+      username TEXT DEFAULT 'Anonim',
+      image_url TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+
+    console.log("Tabela 'posts' gotowa.");
+  } catch (err) {
+    console.error("Błąd podczas tworzenia tabeli:", err);
+    process.exit(1); // zakończ, jeśli nie możesz stworzyć tabeli
+  }
+})();
+
+// API bloga
+app.get("/posts", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM posts ORDER BY created_at DESC");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/posts", async (req, res) => {
+  const { title, content, username = "Anonim", image_url = null } = req.body;
+  if (!title || !content) return res.status(400).json({ error: "Missing title or content" });
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO posts (title, content, username, image_url)
+       VALUES ($1, $2, $3, $4)
+       RETURNING *`,
+      [title, content, username || "Anonim", image_url]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+const server = app.listen(port, () => console.log(`App listening on port ${port}!`));
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Backend dziala!
+    </section>
+  </body>
+</html>
+`
